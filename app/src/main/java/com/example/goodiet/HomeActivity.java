2c139fcec63b6141e5032b9b5bc01f0801d708ff
RubenNotario <71195436+RubenNotario@users.noreply.github.com>
2021-05-15 02:29:37 +0200

    Cambio a Grid View, cambio de action bar en home, fondo sin opacidad de iconos y otros cambios menores.

--- app/src/main/java/com/example/goodiet/HomeActivity.java
+++ app/src/main/java/com/example/goodiet/HomeActivity.java
@@ -7,12 +7,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    ListView listaCategorias;
+    GridView listaCategorias;
     Categoria[] categorias;
 
     @Override
@@ -20,16 +21,17 @@ public class HomeActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        categorias = new Categoria[9];
-        categorias[0] = new Categoria("Nombre de Categoria", R.mipmap.ic_launcher);
-        categorias[1] = new Categoria("Nombre de Categoria", R.mipmap.ic_launcher);
-        categorias[2] = new Categoria("Nombre de Categoria", R.mipmap.ic_launcher);
-        categorias[3] = new Categoria("Nombre de Categoria", R.mipmap.ic_launcher);
-        categorias[4] = new Categoria("Nombre de Categoria", R.mipmap.ic_launcher);
-        categorias[5] = new Categoria("Nombre de Categoria", R.mipmap.ic_launcher);
-        categorias[6] = new Categoria("Nombre de Categoria", R.mipmap.ic_launcher);
-        categorias[7] = new Categoria("Nombre de Categoria", R.mipmap.ic_launcher);
-        categorias[8] = new Categoria("Nombre de Categoria", R.mipmap.ic_launcher);
+        categorias = new Categoria[10];
+        categorias[0] = new Categoria("Vegana", R.drawable.logo);
+        categorias[1] = new Categoria("Sin gluten", R.drawable.logo);
+        categorias[2] = new Categoria("Alta en Proteina", R.drawable.logo);
+        categorias[3] = new Categoria("Baja en grasa", R.drawable.logo);
+        categorias[4] = new Categoria("China", R.drawable.logo);
+        categorias[5] = new Categoria("Pastas", R.drawable.logo);
+        categorias[6] = new Categoria("India", R.drawable.logo);
+        categorias[7] = new Categoria("Rápidas", R.drawable.logo);
+        categorias[8] = new Categoria("Nombre de Categoria", R.drawable.logo);
+        categorias[9] = new Categoria("Nombre de Categoria", R.drawable.logo);
 
         listaCategorias = findViewById(R.id.listaCategorias);
 
