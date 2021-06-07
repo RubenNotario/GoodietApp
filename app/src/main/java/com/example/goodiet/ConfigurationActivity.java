860bf1af087a3ebbc101073b4387e6cf5734269d
unai1997 <82450327+unai1997@users.noreply.github.com>
2021-06-07 14:01:12 +0200

    despensa y lista de compra

--- app/src/main/java/com/example/goodiet/ConfigurationActivity.java
+++ app/src/main/java/com/example/goodiet/ConfigurationActivity.java
@@ -7,16 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 
 public class ConfigurationActivity extends AppCompatActivity {
-
+    String token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
-
+        token = getIntent().getStringExtra("token");
     }
 
     public void Atras(View view) {
         Intent intent = new Intent(ConfigurationActivity.this, HomeActivity.class);
+        intent.putExtra("token" , token);
         startActivity(intent);
         finish();
     }
